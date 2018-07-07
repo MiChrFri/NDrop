aeca35391c8dbfa40c864387d45ccff97533afb0
Michael Frick <michaelfrick@thoughtworks.com>
2018-07-07 10:46:25 +0200

    Initial Commit

---
+++ NDrop WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  NDrop WatchKit Extension
+//
+//  Created by Michael Frick on 07/07/2018.
+//  Copyright © 2018 Michael Frick. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
